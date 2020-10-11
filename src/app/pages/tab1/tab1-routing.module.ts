232fafd572a5414aad887add5a2d717abb5be1c4
Walter Lemus <walter.lemus@hotmail.com>
2020-10-11 10:13:39 -0600

    commit inicial

--- src/app/pages/tab1/tab1-routing.module.ts
+++ src/app/pages/tab1/tab1-routing.module.ts
@@ -6,7 +6,12 @@ const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
-  }
+  },
+  {
+    path: 'agregar/:listaId',
+    loadChildren: () =>
+      import('../agregar/agregar.module').then((m) => m.AgregarPageModule),
+  },
 ];
 
 @NgModule({
---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  listas: any[] = [];
+
+  constructor() {
+
+    this.cargarStorage();
+//    const lista1 = new Lista('Recolectar piedras del infinito');
+//    const lista2 = new Lista('Heroes a desaparecer');
+
+//    this.listas.push(lista1, lista2);
+  }
+
+  crearLista( titulo: string){
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+
+    return nuevaLista.id;
+  }
+
+  obtenerLista(id: string | number){
+    id = Number(id);
+
+    return this.listas.find( listasData => listasData.id === id);
+  }
+
+  guardarStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage(){
+    if (localStorage.getItem('data')){
+      this.listas = JSON.parse(localStorage.getItem('data'));
+      console.log(this.listas);
+    }else{
+      this.listas = [];
+    }
+  }
+}
